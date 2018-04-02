fd1f3c54436bbf4ed26d7ab5e4185456ceae36d6
Taotaot <37956922+Taotaot@users.noreply.github.com>
2018-04-02 22:04:58 +0800

    Create diedai.py

---
+++ diedai.py
@@ -0,0 +1,12 @@
+＃！/ usr / bin / env python3
+＃ -  *  - 编码：utf-8  -  *  -
+
+打印（[X * X 为 X 在 范围（1，11）]）
+打印（[X * X 为 X 在 范围（1，11）如果 X ％ 2  ==  0 ]）
+打印（[M + Ñ 为米在 ' ABC ' 为 ñ 在 ' XYZ ' ]）
+
+d = { ' x '：' A '，' y '：' B '，' z '：' C ' }
+print（[k +  ' = '  + v for k，v in d.items（）]）
+
+L = [ '你好'，'世界'，' IBM '，'苹果' ]
+print（[s.lower（）for s in L]）
